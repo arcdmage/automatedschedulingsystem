7364ef009bd5bcc55b40675112922d622027ffcc
arc <markandrieblancosantos@gmail.com>
2025-10-31 00:09:47 +0800

    split divs into individual php files under "tabs" folder

---
+++ tabs/faculty_schedules.php
@@ -0,0 +1,2 @@
+    <h1>Global Schedules</h1>
+    <p>Shows all the schedules of faculty which can only be seen and edited by ADMINS</p>
---
+++ tabs/subject_list.php
@@ -0,0 +1,2 @@
+    <h1>List of Subjects</h1>
+    <p>Shows the list of subjects, categorized by grade level and STRAND.</p>
