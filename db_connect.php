48952afd85f77b15d4306214b45a2340fcbbc75e
arc <markandrieblancosantos@gmail.com>
2025-10-31 23:00:52 +0800

    changed database to db_connect

--- db_connect.php
+++ db_connect.php
@@ -1,15 +1,14 @@
  <?php
-$servername = "localhost"; // db address or name
+$servername = "localhost"; // db address or host
 $database = "main"; // database name
 $username = "root"; // database username
 $password = ""; // database password
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $database);date_get_last_errors(); // Create connection
+$conn = new mysqli($servername, $username, $password, $database); // Create connection
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error); // terminate script if connection fails
 }
-echo "Connected successfully to ", $database; // confirm successful connection
 ?> 
