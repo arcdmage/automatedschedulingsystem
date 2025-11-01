087e873444067aae5c4fd1eb17318876a46c1903
arc <markandrieblancosantos@gmail.com>
2025-11-01 22:50:14 +0800

    improve comments for clarity in faculty_members.js

--- tabs/faculty_members.js
+++ tabs/faculty_members.js
@@ -1,3 +1,6 @@
+// Loads faculty data into the table via AJAX and updates the table content
+/* to be honest I really still don't understand AJAX lol but thanks to open-sources 
+ i can somewhat understand it now :D */
 function loadFaculty(page = 1, limit = 10) {
   fetch(`tabs/faculty_members.php?page=${page}&limit=${limit}`)
     .then(response => response.text())
