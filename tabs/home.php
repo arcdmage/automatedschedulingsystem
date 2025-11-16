ce94a5d922581092b6812773b2e80a04ce659d12
arc <markandrieblancosantos@gmail.com>
2025-11-16 23:33:46 +0800

    feat: move Google Maps iframe to home tab for improved location visibility

--- tabs/home.php
+++ tabs/home.php
@@ -1,2 +1,8 @@
     <h1>Home</h1>
     <p>Will show global datas including time&date, current events and upcoming events</p>
+    <link rel="stylesheet" href="/mainscheduler/styles.css">
+    <div class="googlemap"> 
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3897.241406248389!2d121.0579418752357!3d12.366755387895779!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bb03405c456aab%3A0x781129caa5d5e5ee!2sSan%20Jose%20National%20High%20School!5e0!3m2!1sen!2sph!4v1763306682561!5m2!1sen!2sph" 
+                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+        </div>
