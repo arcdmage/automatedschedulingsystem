2db0b46e81548d17c7a602c505309d815f353839
arcdmage <markandrieblancosantos@gmail.com>
2026-03-29 05:40:40 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -5,9 +5,10 @@ $servername = "db.fr-pari1.bengt.wasmernet.com"; // db address or host
 $database = "db8WqMRmz97PJdi4SJsjGmVZ"; // database name
 $username = "843b3d48797a800056eef0c5fc31"; // database username
 $password = "069c843b-3d48-7a90-8000-3f9fd4c5f214"; // database password
+$port = 10272; // database port
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $database); // Create connection
+$conn = new mysqli($servername, $username, $password, $database, $port); // Create connection
 
 // Check connection
 if ($conn->connect_error) {
