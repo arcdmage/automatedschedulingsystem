eab5425008759786f40e792a9e34a9d902e9841f
arcdmage <markandrieblancosantos@gmail.com>
2026-03-29 05:34:04 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "db.fr-pari1.bengt.wasmernet.comt"; // db address or host
+$servername = "db.fr-pari1.bengt.wasmernet.com"; // db address or host
 $database = "db8WqMRmz97PJdi4SJsjGmVZ"; // database name
 $username = "843b3d48797a800056eef0c5fc31"; // database username
 $password = "069c843b-3d48-7a90-8000-3f9fd4c5f214"; // database password
