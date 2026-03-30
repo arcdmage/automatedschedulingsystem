54befee899fab69a2429fd22f55e8e71e7c50973
arcdmage <markandrieblancosantos@gmail.com>
2026-03-30 10:31:47 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost"; // db address or host
-$database = "main"; // database name
-$username = "root"; // database username
-$password = ""; // database password
+$servername = "db.fr-pari1.bengt.wasmernet.comt"; // db address or host
+$database = "db8WqMRmz97PJdi4SJsjGmVZ"; // database name
+$username = "843b3d48797a800056eef0c5fc31"; // database username
+$password = "069c843b-3d48-7a90-8000-3f9fd4c5f214"; // database password
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database); // Create connection
