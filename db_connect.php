8595d11b7a86271ed1c5d3bcfd6cf3a08fad06c9
arcdmage <markandrieblancosantos@gmail.com>
2026-03-30 10:52:51 +0800

    Configure local database credentials

--- db_connect.php
+++ db_connect.php
@@ -1,14 +1,15 @@
 <?php
-$servername = "db.fr-pari1.bengt.wasmernet.comt"; // db address or host
-$database = "db8WqMRmz97PJdi4SJsjGmVZ"; // database name
-$username = "843b3d48797a800056eef0c5fc31"; // database username
-$password = "069c843b-3d48-7a90-8000-3f9fd4c5f214"; // database password
+$servername = "localhost"; // db address or host
+$database = "main"; // database name
+$username = "root"; // database username
+$password = ""; // database password
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database); // Create connection
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error); // terminate script if connection fails
+    throw new Exception("Connection failed: " . $conn->connect_error);
 }
+
 ?>
